0a5145abf035eb4713fad139f8df7c0d09ebd969
Clervens Volcy <clervens.volcy@gmail.com>
2019-01-13 15:08:57 -0500

    Add static/hardcoded env varible

--- date.js
+++ date.js
@@ -1,3 +1,3 @@
 module.exports = (req, res) => {
-    res.end(`The date is ${Date.now()} : ${process.env.MY_EMAIL}`);
+    res.end(`The date is ${Date.now()} : ${process.env.MY_NAME} <${process.env.MY_EMAIL}>`);
 }
