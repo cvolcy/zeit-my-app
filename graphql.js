da2af2179f08088384e1c901c30a884e771713f1
Clervens Volcy <clervens.volcy@inovva.com>
2019-01-22 09:39:42 -0500

    add comments

--- graphql.js
+++ graphql.js
@@ -1,10 +1,14 @@
 const { parse } = require('url');
 const { graphql } = require("graphql");
 const mongoose = require('mongoose');
+
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection
     .on('error', (error) => { console.warn('Warning', error); });
+
+// Load Mongoose models
 require('./src/graphql/models/models.js');
+
 const schema = require("./src/graphql/schema.js");
 
 module.exports = async (req, res) => {
